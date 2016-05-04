83016d613fed944dfa3081b4ce34d3c1ab1a9676
Bill Fulton <Bill Fulton>
2016-05-04 11:21:04 -0400

    Add day program

---
+++ daything/App.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "dayType.h"
+using namespace std;
+
+int main()
+{
+
+	dayType someDay;
+
+	 dayType testDayOne;
+	 dayType testDayTwo(3);
+
+	 testDayOne.print();
+	 testDayTwo.print();
+
+	 cout << "The day after " << testDayOne.getDay() << " is " << testDayOne.nextDay() << endl;
+	 cout << "The day before " << testDayOne.getDay() << " is " << testDayOne.previousDay() << endl;
+	 cout << "Five days after " << testDayOne.getDay() << " it will be " << testDayOne.addDays(5) << endl;
+
+
+	cin.get();
+	return 0;
+}
